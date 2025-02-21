5e3deb7051523506d021e3e9985a844a5c643133
Tomas Oficina <damj03.2024@gmail.com>
2025-02-21 11:52:03 +0100

    BottomAppBar funciona

--- app/src/main/java/com/example/pruebastfg/ui/AppMainScreen.kt
+++ app/src/main/java/com/example/pruebastfg/ui/AppMainScreen.kt
@@ -1,6 +1,5 @@
 package com.example.pruebastfg.ui
 
-import android.view.View
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
@@ -20,6 +19,7 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -44,7 +44,10 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavHostController
@@ -55,6 +58,7 @@ import androidx.navigation.compose.rememberNavController
 import com.example.pruebastfg.R
 import com.example.pruebastfg.ui.data.storage.AppViewModelFactory
 import com.example.pruebastfg.ui.data.storage.PreferencesRepository
+import com.example.pruebastfg.ui.theme.Purple40
 import kotlinx.coroutines.launch
 
 enum class AppScreens(@StringRes val title: Int) {
@@ -123,6 +127,7 @@ fun MainScreen(
     val apps = viewModel.getInstalledApps(LocalContext.current)
 
     Scaffold(
+        containerColor = MaterialTheme.colorScheme.surface,
         topBar = {
             AppTopBar(
                 currentScreen = currentScreen,
@@ -130,6 +135,15 @@ fun MainScreen(
                 navigateUp = { navController.popBackStack() },
                 modifier = Modifier,
                 "$userName"
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                currentScreen = currentScreen,
+                navController = navController,
+                canNavigateBack = navController.previousBackStackEntry != null,
+                navigateUp = { navController.popBackStack() },
+                modifier = Modifier
             )
         }
     ) { innerPadding ->
@@ -175,39 +189,54 @@ fun MainScreen(
 
             }
         }
-//        Column(
-//            modifier = Modifier
-//                .fillMaxSize()
-//                .padding(innerPadding)
-//        ) {
-//
-//            // Input para escribir el nombre
-//            NameInput(
-//                userName = uiState.userName,
-//                onNameChange = { viewModel.updateUserName(it) },
-//                btnSaveOnClick = {
-//                    viewModel.viewModelScope.launch {
-//                        repository.saveUserName(
-//                            uiState.userName,
-//                            context
-//                        ) // Guarda el nombre correctamente
-//                    }
-////                    viewModelScope.launch:
-////                    viewModelScope is a CoroutineScope tied to the ViewModel.
-////                    It automatically cancels any running coroutines when the ViewModel is cleared
-////                    (e.g., when the Activity/Fragment is destroyed).
-////                    launch is used to start a new coroutine for non-blocking tasks.
-//                },
-//                btnClearOnClick = {
-//                    viewModel.viewModelScope.launch {
-//                        repository.clearUserName() // Borra el nombre correctamente
-//                    }
-//                }
-//            )
-//
-//            // Mostrar la lista de aplicaciones
-//            AppsListScreen(apps = apps, onAppClick = onAppClick)
     }
+}
+
+@Composable
+fun BottomAppBar(
+    currentScreen: AppScreens,
+    navController: NavHostController,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    androidx.compose.material3.BottomAppBar(
+        modifier = modifier.fillMaxWidth(),
+        containerColor = MaterialTheme.colorScheme.surface,
+        tonalElevation = 0.dp,
+        contentColor = MaterialTheme.colorScheme.primaryContainer,
+        actions = {
+            if (canNavigateBack) {
+                Card(
+                    modifier = Modifier
+                        .clickable {navigateUp()}
+                        .padding(10.dp)
+                        .fillMaxSize()
+                )
+                {
+                    Row(
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.fillMaxSize(),
+
+                        ) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(R.string.back_button)
+                        )
+                        Text(
+                            text = "Atrás",
+                            textAlign = TextAlign.Center,
+                            fontSize = 20.sp,
+                            modifier = Modifier.padding(10.dp)
+
+                        )
+                    }
+
+                }
+            }
+        }
+    )
 }
 
 @Composable
@@ -266,13 +295,13 @@ fun AppsListScreen(apps: List<AppModel>, onAppClick: (String) -> Unit) {
         verticalArrangement = Arrangement.Top
     ) {
         items(apps) { app ->
-            AppListItem(app = app, onClick = { onAppClick(app.packageName) })
+            AppItem(app = app, onClick = { onAppClick(app.packageName) })
         }
     }
 }
 
 @Composable
-fun AppListItem(app: AppModel, onClick: () -> Unit) {
+fun AppItem(app: AppModel, onClick: () -> Unit) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
